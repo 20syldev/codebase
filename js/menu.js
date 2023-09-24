237fe89bb4a6148918404af4c976db10f3bc8705
20syl <20syl.lam78@gmail.com>
2023-09-24 18:13:09 +0200

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -1,155 +1,3 @@
-var commandsColumnVisible1, commandsColumnVisible2, commandsColumnVisible3, commandsColumnVisible4 = true;
-
-function toggleCommandsColumn1() {
-  var commandsColumn = document.getElementById("commands-column1");
-  var toggleButton = document.querySelector(".toggle1");
-
-  if (commandsColumnVisible1) {
-    commandsColumn.style.display = "none";
-    toggleButton.classList.remove("rotate");
-    toggleButton.classList.add("reverse");
-  } else {
-    commandsColumn.style.display = "block";
-    toggleButton.classList.remove("reverse");
-    toggleButton.classList.add("rotate");
-  }
-
-  commandsColumnVisible1 = !commandsColumnVisible1;
-
-  localStorage.setItem('selectedMenu1', JSON.stringify(commandsColumnVisible1));
-}
-
-document.addEventListener('DOMContentLoaded', function() {
-  var selectedMenu1 = localStorage.getItem('selectedMenu1');
-  if (selectedMenu1 !== null) {
-    selectedMenu1 = JSON.parse(selectedMenu1);
-    if (selectedMenu1) {
-      toggleCommandsColumn1();
-    }
-  }
-});
-
-
-function toggleCommandsColumn2() {
-  var commandsColumn = document.getElementById("commands-column2");
-  var toggleButton = document.querySelector(".toggle2");
-
-  if (commandsColumnVisible2) {
-    commandsColumn.style.display = "none";
-    toggleButton.classList.remove("rotate");
-    toggleButton.classList.add("reverse");
-  } else {
-    commandsColumn.style.display = "block";
-    toggleButton.classList.remove("reverse");
-    toggleButton.classList.add("rotate");
-  }
-
-  commandsColumnVisible2 = !commandsColumnVisible2;
-
-  localStorage.setItem('selectedMenu2', JSON.stringify(commandsColumnVisible2));
-}
-
-document.addEventListener('DOMContentLoaded', function() {
-  var selectedMenu2 = localStorage.getItem('selectedMenu2');
-  if (selectedMenu2 !== null) {
-    selectedMenu2 = JSON.parse(selectedMenu2);
-    if (selectedMenu2) {
-      toggleCommandsColumn2();
-    }
-  }
-});
-
-
-function toggleCommandsColumn3() {
-  var commandsColumn = document.getElementById("commands-column3");
-  var toggleButton = document.querySelector(".toggle3");
-
-  if (commandsColumnVisible3) {
-    commandsColumn.style.display = "none";
-    toggleButton.classList.remove("rotate");
-    toggleButton.classList.add("reverse");
-  } else {
-    commandsColumn.style.display = "block";
-    toggleButton.classList.remove("reverse");
-    toggleButton.classList.add("rotate");
-  }
-
-  commandsColumnVisible3 = !commandsColumnVisible3;
-
-  localStorage.setItem('selectedMenu3', JSON.stringify(commandsColumnVisible3));
-}
-
-document.addEventListener('DOMContentLoaded', function() {
-  var selectedMenu3 = localStorage.getItem('selectedMenu3');
-  if (selectedMenu3 !== null) {
-    selectedMenu3 = JSON.parse(selectedMenu3);
-    if (selectedMenu3) {
-      toggleCommandsColumn3();
-    }
-  }
-});
-
-
-function toggleCommandsColumn4() {
-  var commandsColumn = document.getElementById("commands-column4");
-  var toggleButton = document.querySelector(".toggle4");
-
-  if (commandsColumnVisible4) {
-    commandsColumn.style.display = "none";
-    toggleButton.classList.remove("rotate");
-    toggleButton.classList.add("reverse");
-  } else {
-    commandsColumn.style.display = "block";
-    toggleButton.classList.remove("reverse");
-    toggleButton.classList.add("rotate");
-  }
-
-  commandsColumnVisible4 = !commandsColumnVisible4;
-
-  localStorage.setItem('selectedMenu4', JSON.stringify(commandsColumnVisible4));
-}
-
-document.addEventListener('DOMContentLoaded', function() {
-  var selectedMenu4 = localStorage.getItem('selectedMenu4');
-  if (selectedMenu4 !== null) {
-    selectedMenu4 = JSON.parse(selectedMenu4);
-    if (selectedMenu4) {
-      toggleCommandsColumn4();
-    }
-  }
-});
-
-
-function switchTheme(theme) {
-  var leftMenu = document.getElementById('left-menu');
-  var bodycolor = document.getElementById('body-color');
-  var footer = document.getElementById('footer');
-  var bar1 = document.getElementById('bar1');
-  var bar2 = document.getElementById('bar2');
-
-  leftMenu.classList.remove('theme1', 'theme2', 'theme3');
-  bodycolor.classList.remove('theme1', 'theme2', 'theme3');
-  footer.classList.remove('theme1', 'theme2', 'theme3');
-  bar1.classList.remove('theme1', 'theme2', 'theme3');
-  bar2.classList.remove('theme1', 'theme2', 'theme3');
-  
-  leftMenu.classList.add(theme);
-  bodycolor.classList.add(theme);
-  footer.classList.add(theme);
-  bar1.classList.add(theme);
-  bar2.classList.add(theme);
-
-  localStorage.setItem('selectedTheme', theme);
-}
-
-document.addEventListener('DOMContentLoaded', function() {
-  var selectedTheme = localStorage.getItem('selectedTheme');
-  if (selectedTheme) {
-    switchTheme(selectedTheme);
-  }
-});
-
-
 function copyContent(button) {
   try {
       const copyText = button.getAttribute("data-link");
@@ -181,3 +29,23 @@ function copyContent(button) {
       console.error("Échec de la copie : ", err);
   }
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+  const viewCounts = [];
+  for (let i = 0; i < 100; i++) {
+    viewCounts[i] = 0;
+  }
+
+  function incrementAndView(index) {
+    viewCounts[index]++;
+    localStorage.setItem(`viewCount${index}`, viewCounts[index]);
+    document.getElementById(`viewCount${index}`).textContent = viewCounts[index];
+  }
+
+  for (let i = 0; i < 100; i++) {
+    const bouton = document.getElementById(`linkCode${i}`);
+    bouton.addEventListener('click', () => {
+      incrementAndView(i);
+    });
+  }
+});
